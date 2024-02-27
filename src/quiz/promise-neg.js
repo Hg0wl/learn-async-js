3c1a877f5a81fd98d8725979767129d07b7add1a
drewhermes <hermes.a@northeastern.edu>
2024-02-27 17:01:37 -0500

    started promise-sum and promise-neg

--- src/quiz/promise-neg.js
+++ src/quiz/promise-neg.js
@@ -3,3 +3,29 @@ const array2D = [
     [4, 5, 6],
     [7, 8, -9]
 ];
+
+
+function sum2DArray(arr) {
+    return new Promise((resolve, reject) => {
+        return new Promise((resolve, reject) =>
+        {
+            if (arr.length > rowIdx) {
+                setTimeout(() => {
+                    arr[rowidx].filter((e) => {
+                        return e < 0;
+                    })
+                }, 0);
+            }
+        })
+    });
+}
+
+negsPerRowPromies = [];
+
+for (let x = 0; )
+
+Promise.any(negsPerRowPromies)
+    .then((results) => {
+        console.log(results);
+    })
+    .catch((error) => console.log('Error Msg: $(error)'));
--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -3,3 +3,43 @@ const array2D = [
     [4, 5, 6],
     [7, 8, 9]
 ];
+
+function sumOfARow(arr, rowId) {
+    return new Promise((resolve, reject) => {
+        console.log('Sum called ... ');
+        if(Array.isArray(arr)) {
+           arr.forEach((row) => {return new Promise((resolve, reject) =>
+            {
+                let sum = 0;
+                for (let i = 0; i < row.length; i++) {
+                    sum += row[i];
+                }
+                return sum;
+            })});
+            resolve(sum);
+        }
+        else {
+            console.log('rejecting ... ');
+            reject('BAD INPUT: Expected array as input');
+        }
+        console.log('returning from sum');
+    });
+}
+
+
+
+
+rowSumPromises = []
+for (let x - 0; x < array2D.length; x++) {
+    rowSumPromises.push(sumOfARow(array2D, x))
+}
+
+Promise.all(rowSumPromises)
+    .then((rowSums) => {
+        let sum - 0;
+        rowSums.forEach(rowSum => {
+            sum += rowSum;
+        });
+        console.log('Sum = $(sum)');
+    })
+    .catch((error) => console.log("Error Msg: $(error)"));
